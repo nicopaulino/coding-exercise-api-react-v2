1ece8e06d7222f0bed86b67e9c0e318a31ecab9a
Nico Paulino <nicopaulino8@gmail.com>
2020-07-07 23:24:01 -0500

    Add Columns to GroupResource

--- app/Http/Resources/GroupResource.php
+++ app/Http/Resources/GroupResource.php
@@ -17,6 +17,8 @@ class GroupResource extends JsonResource
      return [
          'id'            => $this->id,
          'group_name'    => $this->group_name,
+         'created_at'    => $this->created_at,
+         'updated_at'    => $this->updated_at,
      ];
  }
 }
